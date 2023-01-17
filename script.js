51e51814267a97f7f516f2af5ea29e86102457da
xMaedayx <120540407+xMaedayx@users.noreply.github.com>
2023-01-16 21:43:36 -0700

    how do I add the special character the user inputted ?

--- script.js
+++ script.js
@@ -1,27 +1,60 @@
 // Assignment code here
-var ComputerOptions = ["0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
-var ComputerOptions2 = ["0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"]
-var ComputerOptions3 = [""]
+var num = ['0,1,2,3,4,5,6,7,8,9']
+var lowerc = ['abcdegfhijklmnopqrstuvwxyz']
+var upperc = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+var specialc = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~ "]
+var Generated = [];
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
   function generatePassword {
-  var userInput1 = window.prompt("Allow me inquire about your passphrase, what length would you like it to be (8-128 characters)?")
-  userInput1 = userInput1.parseFloat() /* how do I use the length of this float in the window.alert below? */
-
-  var userInput2 = window.prompt("Would you like special characters in your passphrase (y/n)? ")
+  var userInput1 = numbers(window.confirm("Allow me inquire about your passphrase, what length would you like it to be (8-128 characters)?"))
   
-  var userInput3 = window.prompt("Which special characters would you prefer in your new passphrase? Here are the allowed characters, !#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ")
+  var userInput2 = window.confirm("Would you like special characters in your passphrase (y/n)? ")
   
-  var userInput4 = window.prompt("Would you like to include lowercase, uppercase and, numeric characters? (y/n)")
-
-  if (!Number.isInteger(userInput1)) {
-    return; 
+  var userInput3 = window.confirm("Which special characters would you prefer in your new passphrase? Here are the allowed characters, !#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ")
+  
+  var userInput4 = window.confirm("Would you like to include lowercase, uppercase and, numeric characters? (y/n)")
+ 
+  if userInput2 && userInput 
+  function writePassword() {
+  
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    
+    passwordText.value = password;
+  
   }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+  
 
-  if (userInput2 && userInput4 == 'y') {
+ 
+ /* we want to concat the combinations of inputs from the user to use in the final for loop for the generates password. /* 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  /* if (userInput2 && userInput4 == 'y') {
    var generation1 = Math.floor(Math.random() * ComputerOptions.length )
    var randomized = ComputerOptions[generation1]
    window.alert("New Passphrase:" + randomized + userInput3 )
@@ -37,7 +70,7 @@ var generateBtn = document.querySelector("#generate");
   if (userInput2 == 'n') and (userInput4 == 'y') { 
 
   }
-
+/*
 
 
 
